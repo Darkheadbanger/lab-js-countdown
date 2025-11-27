a1a2383c01f81bd909538d63863445517c6994e3
David Bouhaben <davidbouhaben@yahoo.co.id>
2025-11-27 14:55:55 +0100

    refactor: streamline countdown logic and event listener setup

--- src/index.js
+++ src/index.js
@@ -2,25 +2,30 @@ const DURATION = 10; // 10 seconds
 let remainingTime = DURATION; // Countdown starting from 10
 let timer = null; // Variable to store the interval
 
+let startBtn = document.getElementById("start-btn");
+let timeElement = document.getElementById("time");
 
+let timeCountdown = Number(timeElement.textContent);
 
 // ITERATION 1: Add event listener to the start button
 
+startBtn.addEventListener("click", (event) => {
+  startCountdown();
+});
+
 // Your code goes here ...
-
-
-
 
 // ITERATION 2: Start Countdown
 function startCountdown() {
-  console.log("startCountdown called!");
-
-
+  let countDownInterval = setInterval(() => {
+    timeCountdown--;
+    timeElement.textContent = timeCountdown;
+  }, 1000);
   // Your code goes here ...
+  if(countDownInterval === 0){
+    
+  }
 }
-
-
-
 
 // ITERATION 3: Show Toast
 function showToast(message) {
@@ -28,11 +33,7 @@ function showToast(message) {
 
   // Your code goes here ...
 
-
-
-
   // BONUS: ITERATION 4: TOAST CLOSE BUTTON
 
   // Your code goes here ...
-
 }
