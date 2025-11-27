7fafe2044b797c2b9147326b564ead722acdc0a1
David Bouhaben <davidbouhaben@yahoo.co.id>
2025-11-27 15:44:12 +0100

    refactor: move variable declarations into startCountdown function and pass toast message to showToast

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 const DURATION = 10; // 10 seconds
 let remainingTime = DURATION; // Countdown starting from 10
 let timer = null; // Variable to store the interval
-
 let startBtn = document.getElementById("start-btn");
-let timeElement = document.getElementById("time");
-
-let timeCountdown = Number(timeElement.textContent);
-let countDownInterval = 0;
 
 // ITERATION 1: Add event listener to the start button
 
@@ -19,12 +14,19 @@ startBtn.addEventListener("click", (event) => {
 
 // ITERATION 2: Start Countdown
 function startCountdown() {
+  let timeElement = document.getElementById("time");
+
+  let timeCountdown = Number(timeElement.textContent);
+  let countDownInterval = 0;
+
+  const toastMessage = document.getElementById("toast");
+
   countDownInterval = setInterval(() => {
     timeCountdown--;
     timeElement.textContent = timeCountdown;
 
     if (timeCountdown <= 0) {
-      showToast();
+      showToast(toastMessage);
       clearInterval(countDownInterval);
     }
   }, 1000);
@@ -35,6 +37,7 @@ function showToast(message) {
   console.log("showToast called!");
 
   // Your code goes here ...
+  message.classList.add("show");
 
   // BONUS: ITERATION 4: TOAST CLOSE BUTTON
 
